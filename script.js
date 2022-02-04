698ef836087c6b0f2c6ab2f37346014d9c0cd06d
Lavina malani <94512210+Lavina124@users.noreply.github.com>
2022-02-04 11:47:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+var myNode = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNode.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNode[i].appendChild(span);
+}
+
+// Create a new list item when clicking on the "Add" button
+function MyElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myIn").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("PLEASE ADD SOMETHING");
+  } else {
+    document.getElementById("myli").appendChild(li);
+  }
+  document.getElementById("myIn").value = "";
+
+  var span = document.createElement("SPAN");
+  var text = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(text);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
